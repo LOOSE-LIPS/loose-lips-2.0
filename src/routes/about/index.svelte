9d641d16056161438f8ac5c37da941bba44632ae
natesheehan <nathanaelsheehan@gmail.com>
2023-01-18 01:33:07 +0000

    tidy up crew

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -28,22 +28,6 @@
 
 <!-- Start: About page section -->
 <div class="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-	<h1 class="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white"> LOOSE LIPS </h1>
-	<div class="mb-8 prose leading-6 text-gray-600 dark:text-gray-400">
-		<p>
-			LOOSELIPS. I'm a Software Developer, writer and creator of
-			<ExternalLink href="https://github.com/navneetsharmaui/sveltkit-starter"
-				>Sveltekit Starter Project</ExternalLink
-			> and <ExternalLink href="https://github.com/navneetsharmaui/sveltkit-starter"
-				>Sveltekit Blog Project</ExternalLink
-			>
-		</p>
-		<p>
-			The template has all the route and sample pages setup for you which you can change and customize further.
-			All the pages are by default loaded from the different JS file and is fetched using prefetch when use hover
-			over the links, which reduces the load time when user eventually decides to click on the link and load the
-			route
-		</p>
-	</div>
+	<h1 class="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white"> Coming soon </h1>
 </div>
 <!-- End: About page section -->
