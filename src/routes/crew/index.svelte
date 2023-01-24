2c2fd2c481122f69cf3bf412251db3a4205adc68
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-01-24 00:31:36 +0000

    crew Page styling

--- src/routes/crew/index.svelte
+++ src/routes/crew/index.svelte
@@ -45,37 +45,36 @@
 <HeadTags {metaData} />
 
 <div class="pt-[5%]">
-  <h1
-    class="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white pl-[10%]"
-  >
-    Crew
-  </h1>
-
-  <div class="relative w-full mb-4">
-    <input
-      bind:value={searchValue}
-      aria-label="Search articles"
-      type="text"
-      placeholder="Search Crew members"
-      class="px-4 py-2 border border-gray-300 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
-    />
-    <svg
-      class="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width={2}
-        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-      />
-    </svg>
-  </div>
   <div class="pl-[25%] pr-[25%] pt-[4%]">
-    <div class="flex flex-row">
+    <h1
+      class="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white"
+    >
+      Crew
+    </h1>
+    <div class="mb-4">
+      <input
+        bind:value={searchValue}
+        aria-label="Search articles"
+        type="text"
+        placeholder="Search Crew members"
+        class="px-4 py-2 border border-gray-300 dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 block w-full rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+      />
+      <svg
+        class="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width={2}
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </div>
+    <div class="flex flex-row ">
       {#if !searchValue}
         {#each crewMembers as crewMember, index (crewMember.slug)}
           <CrewPost {crewMember} />
