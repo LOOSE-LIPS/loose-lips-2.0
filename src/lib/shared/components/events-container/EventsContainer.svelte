923368560b85b2440e7b7f14b7d4d91af8e8051a
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-01-28 15:20:47 +0000

    index page cleanup 2

---
+++ src/lib/shared/components/events-container/EventsContainer.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import ProjectCard from "$components/project-card/ProjectCard.svelte";
+  import type { IProjectCard } from "$models/interfaces/iproject-card.interface";
+  const events: IProjectCard[] = [
+    {
+      title: "Loose Lips presents: Sunil Sharpe, Cersy & Kortzer",
+      description:
+        "Loose Lips brings the legendary Irish turntablist Sunil Sharpe to an exciting new Manchester spot fitted with a beautiful Danley soundsystem. Supported by up and coming techno talent Cersy, and Loose Lips resident Kortzer.",
+      slug: "https://github.com/navneetsharmaui/sveltekit-starter",
+      img:
+        "https://imgproxy.ra.co/_/quality:66/w:1500/rt:fill/aHR0cHM6Ly9pbWFnZXMucmEuY28vODkxMjlmZGEzN2EzZjIxMDEwOTg1YzZiZmNmNjVjZDFlMGI1ZWIwYi5wbmc=",
+      icon: "",
+      date: "28/01/2023",
+    },
+  ];
+</script>
+
+<div class="flex flex-row justify-left items-start max-w-2xl mx-auto">
+  <div class="p-5">
+    <!-- Start: Events -->
+    <h2
+      class="font-bold text-2xl md:text-2xl tracking-tight mb-2 max-w-1xl text-black dark:text-white"
+    >
+      Upcoming Events
+    </h2>
+    {#if events.length > 0}
+      {#each events as event}
+        <ProjectCard project={event} />
+      {/each}
+    {/if}
+    <a href="events" class="viewAll">
+      <p class="font-italic text-m text-white">View past events</p>
+    </a>
+    <!-- End: Top Events -->
+  </div>
+</div>
---
+++ src/lib/shared/components/recent-posts-container/RecentPostsContainer.svelte
@@ -0,0 +1,54 @@
+<script lang="ts" context="module">
+  /**
+   * @type {import('@sveltejs/kit').Load}
+   */
+  export async function load({ fetch }) {
+    return {
+      props: {
+        blogs: await fetch(`/blog.json?recent=${5}`).then((res) => res.json()),
+      },
+    };
+  }
+</script>
+
+<script lang="ts">
+  import BlogPost from "../blog-post/BlogPost.svelte";
+  import type { IBlog } from "$models/interfaces/iblog.interface";
+  export let blogs!: IBlog[];
+  //   export let featuredBlogs;
+
+  let listWithDuplicatetags: string[] = [];
+
+  blogs.forEach((blog) => {
+    listWithDuplicatetags =
+      listWithDuplicatetags.length === 0
+        ? [...blog.tags]
+        : [...listWithDuplicatetags, ...blog.tags];
+  });
+  $: tags = [...new Set(listWithDuplicatetags)];
+
+  const mostRecentBlogs: IBlog[] = blogs
+    .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)))
+    .slice(0, 3);
+</script>
+
+<div
+  class="flex flex-row  items-start max-w-6xl mx-auto mb-16 hover:transform-rotate(4deg)"
+>
+  <!-- Start: Popular Blog Section -->
+  <h2
+    class="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white mt-8em"
+    style="margin-top: 15%;"
+  >
+    Recent Posts
+  </h2>
+  <div />
+  {#if blogs.length > 0}
+    {#each blogs as blog, index (blog.slug)}
+      <div class="p-5">
+        <BlogPost {blog} />
+      </div>
+    {/each}
+  {/if}
+  <!-- End: Popular Blog Section -->
+</div>
