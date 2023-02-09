2bff2d5ac99e5e7f8031d2a24cf95b0cdb80cdee
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-02-09 22:22:12 +0000

    recommended function finitoooo

--- src/lib/shared/components/recommended-posts/RecommendedPostsContainer.svelte
+++ src/lib/shared/components/recommended-posts/RecommendedPostsContainer.svelte
@@ -17,24 +17,22 @@
   import type { IBlog } from "$models/interfaces/iblog.interface";
   import type { IEventsCard } from "$lib/models/interfaces/ievents-card.interface";
   export let posts!: (IBlog | IEventsCard)[];
-  // let recommendedPosts = posts.filter((blog) => {
-  //   return blog.featured;
-  // });
-  export let recommendedPosts = [];
 
+  let recommendedPosts = [];
   recommendedArray.subscribe((data) => {
-    data.map((post) => {
-      recommendedPosts.push(post);
+    posts.forEach((post) => {
+      data.map((tag) => {
+        post.tags.includes(tag)
+          ? recommendedPosts.push(post)
+          : console.log("nope");
+      });
     });
   });
-
-  console.log(recommendedPosts, "test");
 </script>
 
 <div
   class="flex flex-col items-start max-w-6xl mx-auto mb-16 hover:transform-rotate(4deg) "
 >
-  <!-- Start: Popular Blog Section -->
   <h2
     class="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white mt-8em m-6"
     style="margin-top: 15%;"
@@ -43,10 +41,12 @@
   </h2>
 
   <div class="grid grid-flow-col auto-cols-max overflow-x-auto max-w-6xl">
-    {#each recommendedPosts as post}
-      <div>
-        <RecommendedPost {post} />
-      </div>
-    {/each}
+    {#if recommendedPosts.length > 0}
+      {#each recommendedPosts as post}
+        <div>
+          <RecommendedPost {post} />
+        </div>
+      {/each}
+    {/if}
   </div>
 </div>
