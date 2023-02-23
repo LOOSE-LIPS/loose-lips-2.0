28fb104b353615a2a8efa9376f586521beeace1c
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-02-23 23:56:28 +0000

    event page import

--- scripts/testTurndown.js
+++ scripts/testTurndown.js
@@ -2,24 +2,14 @@ import TurndownService from "turndown";
 
 const turndownService = new TurndownService();
 const test = "<a><p><h1> HEYYYY <3<p>  href</p> </h1></p></a>";
-const img = "./something/somthing";
-const test2 =
-  '<iframe style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=879490178/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=1692828727/transparent=true/" seamless="">&lt;a href=&#8221;http://matiasaguayo.bandcamp.com/album/support-alien-invasion-2&#8243;&gt;Support Alien Invasion by Matias Aguayo&lt;/a&gt;</iframe>';
-const test3 = `<img src=${img} alt=image > </img>`;
+const test2 = "<p>>> CLUBFOOT [Strange Riddims] <<</p>";
 
-turndownService.addRule("deleteTagAndContents", {
-  filter: "iframe",
-  replacement: function () {
-    return "";
+turndownService.addRule("test2", {
+  filter: "p",
+  replacement: function (content) {
+    return content.replace(/<<|>>/gi, "");
   },
 });
-
-// turndownService.addRule("test2", {
-//   filter: "h1",
-//   replacement: function (content) {
-//     return content.replace("HEYYYY", "");
-//   },
-// });
 
 // turndownService.addRule("anchor", {
 //   filter: "a",
@@ -28,5 +18,5 @@ turndownService.addRule("deleteTagAndContents", {
 //   },
 // });
 
-const markdownString = turndownService.turndown(`${test3}`);
+const markdownString = turndownService.turndown(`${test2}`);
 console.log(markdownString);
