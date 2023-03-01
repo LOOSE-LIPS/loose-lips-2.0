a6cba3d80361e27aec89762e5c30403308313ccd
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-03-01 19:16:22 +0000

    recommended component fix

--- src/lib/data/data.ts
+++ src/lib/data/data.ts
@@ -1,34 +1,34 @@
-import { UserModel } from '$models/classes';
+import { UserModel } from "$models/classes";
 
 export const userData: UserModel[] = [
-	{
-		name: 'Ross',
-		job: 'Professor',
-		id: 'ross',
-	},
-	{
-		name: 'Chandler',
-		job: 'Processor',
-		id: 'chandler',
-	},
-	{
-		name: 'Joe',
-		job: 'Actor',
-		id: 'joe',
-	},
-	{
-		name: 'Rachel',
-		job: 'Designer',
-		id: 'rachel',
-	},
-	{
-		name: 'Monica',
-		job: 'Chef',
-		id: 'monica',
-	},
-	{
-		name: 'Phoebe',
-		job: 'Masseuse',
-		id: 'phoebe',
-	},
+  {
+    name: "Ross",
+    job: "Professor",
+    id: "ross",
+  },
+  {
+    name: "Chandler",
+    job: "Processor",
+    id: "chandler",
+  },
+  {
+    name: "Joe",
+    job: "Actor",
+    id: "joe",
+  },
+  {
+    name: "Rachel",
+    job: "Designer",
+    id: "rachel",
+  },
+  {
+    name: "Monica",
+    job: "Chef",
+    id: "monica",
+  },
+  {
+    name: "Phoebe",
+    job: "Masseuse",
+    id: "phoebe",
+  },
 ].map((value) => new UserModel().deserialize(value));
--- src/lib/shared/components/recommended-posts/RecommendedPostsContainer.svelte
+++ src/lib/shared/components/recommended-posts/RecommendedPostsContainer.svelte
@@ -1,57 +1,51 @@
-<script lang="ts" context="module">
-  /**
-   * @type {import('@sveltejs/kit').Load}
-   */
-  export async function load({ fetch }) {
-    return {
-      props: {
-        // blogs: await fetch(`src/routes/blog.json?recent=${3}`).then((res) =>
-        //   res.json()
-        // ),
-        test: ["test"],
-      },
-    };
-  }
-</script>
-
 <script lang="ts">
   import RecommendedPost from "../recommended-post/RecommendedPost.svelte";
   import type { IBlog } from "$models/interfaces/iblog.interface";
   import type { IEventsCard } from "$lib/models/interfaces/ievents-card.interface";
-  export let test;
-  console.log(test);
-  // recommendedArray.subscribe((data) => {
-  //   posts.forEach((post) => {
-  //     data.map((tag) => {
-  //       post.tags.includes(tag)
-  //         ? recommendedPosts.push(post)
-  //         : console.log("nope");
-  //     });
-  //   });
-  // });
+  import { onMount } from "svelte";
+  import { beforeUpdate } from "svelte";
+  export let tags;
+
+  let recommendedPosts = [];
+
+  onMount(async () => {
+    await fetch("/blog.json")
+      .then((res) => res.json())
+      .then((res) => {
+        res
+          .filter((post) => {
+            return post.tags.includes(tags[0]);
+          })
+          .map((post) => {
+            recommendedPosts.push(post);
+            recommendedPosts = recommendedPosts;
+          });
+      });
+  });
+  console.log(recommendedPosts);
 </script>
 
-<h1 class="text-white">Recommended</h1>
+{#if recommendedPosts.length > 0}
+  <h1 class="text-white">{recommendedPosts[0].id}</h1>
+{/if}
 
-<!-- {#if recommendedPosts.length > 0}
-  <div
-    class="flex flex-col items-start max-w-6xl mx-auto mb-16 hover:transform-rotate(4deg) "
+<div
+  class="flex flex-col items-start max-w-6xl mx-auto mb-16 hover:transform-rotate(4deg) "
+>
+  <h2
+    class="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white mt-8em m-6"
+    style="margin-top: 15%;"
   >
-    <h2
-      class="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white mt-8em m-6"
-      style="margin-top: 15%;"
-    >
-      Recommended
-    </h2>
+    Recommended
+  </h2>
 
-    <div
-      class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 justify-between gap-5"
-    >
-      {#each recommendedPosts as post}
-        <div>
-          <RecommendedPost {post} />
-        </div>
-      {/each}
-    </div>
+  <div
+    class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 justify-between gap-5"
+  >
+    {#each recommendedPosts as post}
+      <div>
+        <RecommendedPost {post} />
+      </div>
+    {/each}
   </div>
-{/if} -->
+</div>
