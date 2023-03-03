dfdb6b43de0cab12b5d26522494541e7011651ef
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-03-03 15:25:18 +0000

    fix

--- src/routes/crew/index.json.ts
+++ src/routes/crew/index.json.ts
@@ -8,9 +8,7 @@ export async function get({
 }: {
   query: URLSearchParams;
 }): Promise<Partial<{ body: IBlog[]; status: number }>> {
-  const modules = import.meta.glob(
-    "../../routes/markdownfiles/importCrew/**/index.{md,svx,svelte.md}"
-  );
+  const modules = import.meta.glob("./**/index.{md,svx,svelte.md}");
 
   const crewPromises = [];
 
