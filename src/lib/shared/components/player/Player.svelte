19bf71a7002d153e1da4fc6b6ea0d561a27f6887
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-03-16 00:57:59 +0000

    embed = player = cool but still in progress

--- src/lib/shared/components/player/Player.svelte
+++ src/lib/shared/components/player/Player.svelte
@@ -1,8 +1,54 @@
-<script>
+<script lang="ts">
+  import { currentTrack } from "$stores";
+  import { onMount } from "svelte";
+  let widget;
   export let track;
+
+  const getSc = async () => {
+    if ((window as any)?.SC) return (window as any)?.SC;
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    return getSc();
+  };
+
+  const handleSoundCloudWidget = async () => {
+    const sc = await getSc();
+    let iframeElement = document.getElementById("perP");
+    widget = sc.Widget(iframeElement);
+
+    widget.bind(sc.Widget.Events.READY, function () {
+      console.log("LETS GOOOOOOO");
+      if (track != "") {
+        setTimeout(() => {
+          widget.play();
+        }, 5000);
+      }
+    });
+  };
+
+  onMount(() => {
+    if (!document.getElementById("perScript")) {
+      const script = document.createElement("script");
+      script.id = "perScript";
+      script.src = "https://w.soundcloud.com/player/api.js";
+      document.body.appendChild(script);
+      script.onload = () => handleSoundCloudWidget();
+      return;
+    }
+    handleSoundCloudWidget();
+  });
 </script>
 
-<div class="fixed bottom-0 left-0 right-0 bg-white text-black h-24 py-2 px-4">
+<div class="fixed bottom-0 left-0 right-0 bg-white text-black h-36 py-2 px-4">
   <h1>PERMANENT Player</h1>
-  <h3>{track}</h3>
+
+  <iframe
+    id="perP"
+    title="title"
+    width="100%"
+    height="100%"
+    scrolling="no"
+    frameborder="no"
+    allow="autoplay"
+    src={track}
+  />
 </div>
