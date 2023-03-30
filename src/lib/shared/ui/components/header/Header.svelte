07364c968198688ea0a046ceb04b112febffb094
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-03-30 23:53:31 +0100

    test to delete after - loose lips test title

--- src/lib/shared/ui/components/header/Header.svelte
+++ src/lib/shared/ui/components/header/Header.svelte
@@ -2,7 +2,6 @@
   // Start: Local Imports
 
   import type { IHeaderNavLink } from "$models/interfaces/iheader-nav-link.interface";
-  import { NavigationProgressBar } from "$ui/components/navigation-progress-bar";
   import { NavigationBar } from "$ui/components/navigation-bar";
   export let logoImage!: string;
   export let title = "LOOSE LIPS";
@@ -13,6 +12,10 @@
    */
 
   const navLinks: IHeaderNavLink[] = [
+    {
+      path: "/",
+      label: "LL",
+    },
 
     {
       path: "/blog",
