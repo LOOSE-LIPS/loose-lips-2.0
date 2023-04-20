8d1610e325968f7c5d515d07ab7e2737c6331367
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-04-20 17:49:12 +0200

    past events vs upcoming events, limit to 10

--- src/lib/shared/components/upcoming-events-container/UpcomingEventsContainer.svelte
+++ src/lib/shared/components/upcoming-events-container/UpcomingEventsContainer.svelte
@@ -11,7 +11,13 @@
     return inputDate.getTime() > currentDate.getTime();
   };
 
-  const filteredEvents = events.filter((event) => isDateInFuture(event.date));
+  const futureEvents = events.filter((event) => isDateInFuture(event.date));
+  const pastEvents = events
+    .filter((event) => !isDateInFuture(event.date))
+    .sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)))
+    .slice(0, 10);
+
+  console.log(pastEvents);
 </script>
 
 <div class="flex flex-row justify-left items-start mx-auto">
@@ -19,19 +25,32 @@
     <h2
       class="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white  m-4"
     >
-      Events
+      Upcoming Events
     </h2>
     <div
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-between gap-5"
     >
-      {#if filteredEvents.length > 0}
-        {#each filteredEvents as event}
+      {#if futureEvents.length > 0}
+        {#each futureEvents as event}
           <EventPost {event} />
         {/each}
+      {:else}
+        <h1
+          class=" text-1xl md:text-1xl tracking-tight mb-4 text-black dark:text-white  m-4"
+        >
+          There are no upcoming events...
+        </h1>
       {/if}
     </div>
     <a href="events" class="viewAll">
-      <p class="font-italic text-m text-white">View past events</p>
+      <p class="font-italic text-m text-white">Past Events</p>
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-between gap-5"
+      >
+        {#each pastEvents as event}
+          <EventPost {event} />
+        {/each}
+      </div>
     </a>
   </div>
 </div>
