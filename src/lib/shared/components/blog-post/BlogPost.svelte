017bf11141de426799a3cda1398a05fb7809abfb
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-04-26 17:37:22 +0200

    tag

--- src/lib/shared/components/blog-post/BlogPost.svelte
+++ src/lib/shared/components/blog-post/BlogPost.svelte
@@ -11,7 +11,9 @@
   <div class="mb-8 w-full border-b border-gray-100 dark:border-gray-800 pb-5">
     <a data-sveltekit:prefetch href={`../blog/${blog.slug}`} class="w-full">
       <div class="flex flex-col md:flex-row justify-between">
-        <img src={blog.banner} alt="" style="height: 12em; width:100%;" />
+        {#if blog.banner}
+          <img src={blog.banner} alt="" style="height: 12em; width:100%;" />
+        {/if}
       </div>
       <h3
         class="text-lg md:text-xl font-medium mb-2 w-full text-gray-900 dark:text-gray-100"
@@ -22,8 +24,10 @@
     <p class="text-gray-600 dark:text-gray-400">
       <!-- {blog.description.substring(0, 100)}... -->
     </p>
-    <p class="text-gray-600 dark:text-gray-400">Read More...</p>
+    <!-- <p class="text-gray-600 dark:text-gray-400">Read More...</p> -->
     <p class="text-gray-600 dark:text-gray-400">{blog.date}</p>
-    <!-- <TagsContainer tags={blog.tags} /> -->
+    <h4 class=" italic font-thin text-gray-300 text-lg md:text-xl mb-2 w-full ">
+      {blog.tags}
+    </h4>
   </div>
 {/if}
