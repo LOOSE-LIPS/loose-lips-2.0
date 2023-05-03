4e0ad4e32a3d9fdb183dcd924c9de38245a76721
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-05-03 12:12:27 +0200

    blog layout cleanup

--- src/lib/layouts/blog-layout/BlogLayout.svelte
+++ src/lib/layouts/blog-layout/BlogLayout.svelte
@@ -6,32 +6,18 @@
   import HeadTags from "$components/head-tags/HeadTags.svelte";
   import RecommendedPostContainer from "$lib/shared/components/recommended-posts/RecommendedPostsContainer.svelte";
 
-  // Utils
   import { blogTypeDate } from "$utils/date-formatters";
   import { readingTime } from "$lib/utils/reading-time";
   import RecentPostsContainer from "$lib/shared/components/recent-posts-container/RecentPostsContainer.svelte";
   import RecommendedPost from "$lib/shared/components/recommended-post/RecommendedPost.svelte";
 
   export let blogs;
-  // Exports
   export let tags = [];
   export let title = "";
   export let slug = "";
   export let description = "";
-  // export let tags = [];
   export let date = "";
   export let author = "";
-  //   export let previousArticleLink = "";
-  //   export let nextArticleLink = "";
-
-  let readingTimeDuration = "";
-
-  //   const editUrl = `${environment.gitHubConfig.GITHUB_BLOG_EDIT_URL}/${slug}/index.md`;
-  //   const discussUrl = `${
-  //     environment.twitterConfig.TWITTER_SEARCH_URL
-  //   }?q=${encodeURIComponent(
-  //     `https://sveltekit-blog-starter.vercel.app/blog/${slug}`
-  //   )}`;
 
   /**
    * @type {IMetaTagProperties}
@@ -61,7 +47,6 @@
     },
   };
 
-  // Start: Reactive properties
   $: {
     if (title && slug) {
       metaData = {
@@ -90,92 +75,16 @@
       };
     }
   }
-
-  // End: Reactive properties
-
-  // Local Methods
-  //   onMount(() => {
-  //     readingTimeDuration = readingTime(
-  //       `${document.getElementById("blog-conent").textContent}`
-  //     ).time;
-  //   });
 </script>
 
-<!-- Start: Header Tag -->
 <HeadTags {metaData} />
-<!-- End: Header Tag -->
 
 <article
   class="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full"
 >
-  <!-- <h1
-    class="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white"
-  >
-    {title}
-  </h1>
-  <div
-    class="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2"
-  > -->
-  <!-- <div class="flex items-center">
-      <img
-        alt={"Sveltekit Blogger"}
-        src={"/images/author/favicon-32x32.png"}
-        class="rounded-full w-7 h-7"
-      />
-      <p class="text-sm text-gray-700 dark:text-gray-300 ml-2">
-        {author}
-        {" / "}
-        {blogTypeDate(date)}
-      </p>
-    </div>
-    <p class="text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
-      {readingTimeDuration}
-    </p>
-  </div> -->
   <div class="prose dark:prose-dark max-w-none w-full mt-40" id="blog-conent">
     <slot />
   </div>
-
-  <!-- <div class="mt-8">
-    <RecommendedPostsContainer {tags} />
-    <TagsContainer {tags} />
-  </div> -->
-  <!-- <div class="mt-8">
-    <p class="text-sm text-gray-700 dark:text-gray-300 mb-4">
-      {"Share the article on"}
-    </p>
-    <ShareButtons
-      {title}
-      {description}
-      url={`${environment.launchURL}/blog/${slug}`}
-    />
-  </div> -->
-  <!-- <div class="text-sm text-gray-700 dark:text-gray-300 mt-8">
-    <ExternalLink
-      href={discussUrl}
-      ariaLabel={title}
-      cssClasses={"text-sm text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-500"}
-    >
-      {"Discuss on Twitter"}
-    </ExternalLink>
-    {` • `}
-    <ExternalLink
-      href={editUrl}
-      ariaLabel={title}
-      cssClasses={"text-sm text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-500"}
-    >
-      {"Edit on GitHub"}
-    </ExternalLink>
-  </div> -->
-  <!-- {#if previousArticleLink || nextArticleLink}
-    <div class="mt-8 w-full">
-      <NextArticle
-        previousHref={previousArticleLink}
-        nextHref={nextArticleLink}
-      />
-    </div>
-  {/if} -->
-  <!-- </div> -->
 </article>
 <div class="flex flex-col justify-center items-start  mx-auto mb-16 w-full">
   <RecommendedPostContainer {tags} />
